7fca55c99642cfad2fc955929764de5376dbfd57
nihalDelimp <nihal@delimp.com>
2023-05-25 12:49:09 +0530

    Api Changes

--- next.config.js
+++ next.config.js
@@ -3,26 +3,26 @@
 //import style from "./src/style/index.scss"
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    remotePatterns:[
-     {
-      protocol:"https",
-      hostname:"d1zbg1tvj6dkkl.cloudfront.net"
-     },
-     {
-      protocol:"https",
-      hostname:"images.pexels.com"
-     }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d1zbg1tvj6dkkl.cloudfront.net"
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com"
+      }
 
     ]
   },
   env: {
-    LARAVEL_BASEURL: 'http://127.0.0.1:8000',
+    LARAVEL_BASEURL: 'https://dev.cannabiscapitol.com',
   },
 }
 
 const path = require('path');
- 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, './src/style/globals.scss')],
