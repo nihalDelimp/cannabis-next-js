5a325b33d8098f8b2663a93c36f90e5092cf124e
Manish <Manish@delimp.com>
2023-05-26 13:49:28 +0530

    Initial commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,24 +3,32 @@ import Script from 'next/script'
 //import Head from 'next/head';
 export default function Document() {
   
- 
+  const meta = {
+    title: "What is canabis capitol?",
+    description:
+      "canabis  capitol is the next step in the evolution of the internet and, possibly, the organization of society. According to legend, Web 1.0 was the era of decentralized, open protocols, when the majority of internet activity consisted of visiting individual static webpages.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1685052391251-e09402a6b8e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80",
+    type: "just testing ",
+  
+  };
    
   return (
     <Html lang="en">
     
       <Head>
         <title>Create Next App</title>
-        <meta property="og:type" content="What is canabis capitol?" />
+        <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="What is Web3?" />
-        <meta property="og:description" content="canabis  capitol is the next step in the evolution of the internet and, possibly, the organization of society. According to legend, Web 1.0 was the era of decentralized, open protocols, when the majority of internet activity consisted of visiting individual static webpages." />
-        <meta property="og:title" content="What is canabis capitol" />
-        <meta property="og:image" content=      "https://images.unsplash.com/photo-1685052391251-e09402a6b8e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80" />
-       {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.imageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.imageUrl} />
-  */}
+     
       </Head>     
      
 
