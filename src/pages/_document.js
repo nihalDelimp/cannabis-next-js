5c4b970c5c2860a4566494545ac95679f5df12b6
Manish <Manish@delimp.com>
2023-05-26 13:24:12 +0530

    Initial commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,37 @@ import Script from 'next/script'
 
 export default function Document() {
   
+  const meta = {
+    title: "What is canabis capitol?",
+    description:
+      "canabis  capitol is the next step in the evolution of the internet and, possibly, the organization of society. According to legend, Web 1.0 was the era of decentralized, open protocols, when the majority of internet activity consisted of visiting individual static webpages.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1685052391251-e09402a6b8e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80",
+    type: "just testing ",
   
+  };
    
   return (
     <Html lang="en">
-      <Head />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"/>
+      <Head >
+      <Head>
+        <title>Create Next App</title>
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="What is Web3?" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.imageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={meta.twitterHandle} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.imageUrl} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>     
+      </Head>
+
       <body>
         <Main />
         <NextScript />
