ed97d06fb8377ebf0b77fd2d558d5eb2f78f5e20
Manish <Manish@delimp.com>
2023-05-26 14:06:34 +0530

    Initial commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head,Main, NextScript } from 'next/document'
 import Script from 'next/script'
-import Head from 'next/head';
+
 export default function Document() {
   
   const meta = {
